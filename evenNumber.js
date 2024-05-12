ed8b85002683c1f216a11b08f676ee1a5f9784cf
madhankarnekota <madhankumarjnv@gmail.com>
2024-05-12 18:30:19 +0530

    implimented even and linear search logic and updated prime number logic

---
+++ evenNumber.js
@@ -0,0 +1,2 @@
+console.log("first commit of second day");
+console.log("composite number logic")
--- prime.js
+++ prime.js
@@ -1,2 +1,4 @@
 console.log("first commit of first day");
-console.log("prime number logic")
+console.log("prime number logic updated")
+
+console.log("new logic of prime number without errors")
