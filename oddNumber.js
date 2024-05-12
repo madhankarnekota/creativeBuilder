4b85c2461c8456672d8aa63efaf5e2985b77cadc
madhankarnekota <madhankumarjnv@gmail.com>
2024-05-12 18:40:42 +0530

    100 number logic

--- oddNumber.js
+++ oddNumber.js
@@ -1,2 +1,4 @@
 console.log("first commit of third day");
 console.log("odd number logic")
+
+console.log("odd number 100 logic")
--- prime.js
+++ prime.js
@@ -2,3 +2,5 @@ console.log("first commit of first day");
 console.log("prime number logic updated")
 
 console.log("new logic of prime number without errors")
+
+console.log("prime number 100 logic")
